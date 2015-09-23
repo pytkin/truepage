40d7f5bc3ef493ce148b6561de2a900b5c1a352d
Dmitry Pytkin <tutbaker@gmail.com>
2015-09-23 07:00:11 +0600

    Add mobile version to cases.html

--- app/js/app.js
+++ app/js/app.js
@@ -2,14 +2,14 @@ $(function () {
 	'use strict';
 
 	// Галереи клиенты и отзывы
-	$('[data-type="review-gallery"], [data-type="clients-gallery"]').flickity({
+	$('[data-type="review-gallery"], [data-type="clients-gallery"], [data-type="cases-gallery"], [data-type="clients-gallery"]').flickity({
 		pageDots: false,
 		prevNextButtons: false,
 		wrapAround: true
 	});
 
 	// Стрелочки для галерей
-	$('.block.is-reviews, .block.is-clients').on('click', '[data-type="gallery-prev-handler"], [data-type="gallery-next-handler"]', function (event) {
+	$('.block.is-reviews, .block.is-clients, .block.is-cases-videos, .block.is-cases-photos').on('click', '[data-type="gallery-prev-handler"], [data-type="gallery-next-handler"]', function (event) {
 		event.preventDefault();
 		var gallery = $(this).closest('.block').find('.flickity-enabled').data('flickity');
 		switch ($(this).data('type')) {
